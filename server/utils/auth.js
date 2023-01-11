3e72a833977f62aa253b98b17706c97283183d4b
Gordon-Magill <gordon.magill@gmail.com>
2023-01-11 00:22:26 -0800

    auth.js prototyped

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,43 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const secret = process.env.spaceGriftWhaleHunter_SECRET
-const expiration = '2h'
+const secret = process.env.spaceGriftWhaleHunter_SECRET;
+const expiration = "2h";
 
-export function authMiddleware() {
+// Use JWT token embedded in request headers (from authenticated users)
+function authMiddleware({ req, res }) {
+  // Token will be in the header authorization prop in the form of a string "Bearer tokenValue"
+  let token = req.headers.authorization;
+  token = token.split(" ").pop().trim();
 
+  // Return the plain request if it's unauthenticated
+  if (!token) {
+    return req;
+  }
+
+  // Extract the user information from the request token and populate the users attribute
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log("Auth middleware error: Could not verify token");
+  }
+
+  // Always return the request, even if it raised an error
+  // If the user is authenticated, the request will now have req.user populated with
+  // the values embedded by signToken, otherwise that property will be undefined
+  return req;
 }
 
-export function signToken(){
-    
+// Embed username, email, and user's mongoDB _id in a token
+// Used by server.js to add identifying user info to request headers after authentication
+function signToken({ username, email, _id }) {
+  const payload = { username, email, _id };
+  const token = jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  return token;
 }
+
+module.exports = {
+  authMiddleware,
+  signToken,
+};
