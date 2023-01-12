21c3393ef3c5fcdb909d5866b5a8cca8cc869271
CArmstrong2 <whitecrow119@gmail.com>
2023-01-11 19:55:27 -0800

    pseudocode for ship generation, made function for stat generation

---
+++ server/utils/makeShip.js
@@ -0,0 +1,13 @@
+
+//this is pseudocode for now
+class Ship {
+    constructor(stats){
+        this.attackPower = stats.attackPower
+        this.health = stats.health
+        this.armor = stats.armor
+        this.shield = stats.shield
+        this.accuracy = stats.accuracy
+        this.initiative = stats.initiative
+        this.evasion = stats.evasion
+    }
+}
---
+++ server/utils/rollStats.js
@@ -0,0 +1,57 @@
+// randomly generate stats for a ship, monster, or part
+// this generates an integer between min and num
+function getRandomInt(num, min) {
+    return Math.floor(Math.random() * num) + min;
+}
+
+function GenerateStats(type){
+    // types: 0 = ship, 1 = monster, 2 = part
+
+    if(type === 0){
+        // generate values for all 7 stats
+
+        let shipResult = {
+            "attackPower" : getRandomInt(10, 1),
+            "health" : getRandomInt(15, 5),
+            "armor" : getRandomInt(10, 2),
+            "shield" : getRandomInt(20, 4),
+            "accuracy" : getRandomInt(25, 5),
+            "initiative" : getRandomInt(15, 1),
+            "evasion" : getRandomInt(25, 5)
+        }
+
+        return shipResult
+
+    } else if(type === 1){
+
+        let monsterResult = {
+            "attackPower" : getRandomInt(10, 1),
+            "health" : getRandomInt(50, 10),
+            "armor" : getRandomInt(5, 0),
+            "shield" : 0,
+            "accuracy" : getRandomInt(25, 5),
+            "initiative" : getRandomInt(15, 1),
+            "evasion" : getRandomInt(10, 1)
+        }
+        
+        return monsterResult
+
+    } else if(type === 2){
+     
+        // generate values for 1 stat
+        // that stat will determine the part type
+
+        let roll = getRandomInt(6, 0)
+
+        let partResult = {
+            "Part Type" : roll,
+            "Stat Bonus" : roll
+        }
+
+        return partResult
+
+    } else {
+        console.log("Bad argument passed to GenerateStats function!")
+        return
+    }
+}
