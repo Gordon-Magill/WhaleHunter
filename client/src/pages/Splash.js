e00a0e90a3b828a4ae6b2663e9f054fd910c1849
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-11 20:09:31 -0700

    Basic splash content for testing:

---
+++ client/src/pages/Splash.js
@@ -0,0 +1,14 @@
+// Main splash home page for '/'. Login and sign up forms
+
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+
+const Splash = () => {
+  return (
+    <div className="splashMain">
+      <h1>Welcome to Whale Hunter</h1>
+    </div>
+  );
+};
+
+export default Splash;
