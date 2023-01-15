b16cfccb39eedebf4186c2775e94c000c9e517c9
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-15 12:25:45 -0700

    Adding image slider to home page

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -1,9 +1,33 @@
 import React from "react";
 
+// Image carousel for home banner
+import { Carousel } from 'antd';
+
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 export default function Splash() {
   return (
     <div className="splash">
-      <h1>Welcome!</h1>
+      <Carousel effect="fade" autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
     </div>
   );
 }
