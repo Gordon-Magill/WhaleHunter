86208e0825129964899a78b2b42aa610964ae3df
Gordon-Magill <gordon.magill@gmail.com>
2023-01-15 03:42:00 -0800

    Adding basic login mutations

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useMutation } from '@apollo/client'
+
 
 export default function Dashboard() {
+
+  // const [login, {error}] = useMutation(LO)
+
   return (
     <div className="dashboard">
           <h1>Your Dashboard</h1>
