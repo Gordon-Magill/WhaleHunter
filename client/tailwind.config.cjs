570399b9d3e0b658fb00eda482ab92cd987c8782
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-16 19:09:51 -0700

    Adding flowbite config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
