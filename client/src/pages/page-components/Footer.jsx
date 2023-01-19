aaeb89e6fe25ae21749559cc4b68581ebc9ae820
Gordon-Magill <gordon.magill@gmail.com>
2023-01-18 19:17:52 -0800

    Backend now working! Screw you, auth middleware with silent errors

--- client/src/pages/page-components/Footer.jsx
+++ client/src/pages/page-components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer px-4 py-6">
-        <div class="footer-content">
-          <p class="text-sm text-gray-600 text-center">Space Grift: Whale Hunter | Really?</p>
+    <footer className="footer px-4 py-6">
+        <div className="footer-content">
+          <p className="text-sm text-gray-600 text-center">Space Grift: Whale Hunter | Really?</p>
         </div>
       </footer>
   );
