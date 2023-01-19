60de94dd251e7f77ba008fb8b780b59ab42e94be
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-18 17:30:54 -0700

    Basic placeholder layout for battle page started:

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -4,6 +4,10 @@ import { motion } from "framer-motion";
 // import user's ship as attacker
 // import random monster as defender
 
+// Test whale picture
+import whaleBossPic from "../assets/cthulhu_whales/tmp3k8jgtcj.png"
+import playerShipPic from "../assets/old_ships/tmpa1uksn_p.png"
+
 
 export default function Battle() {
   return (
@@ -15,23 +19,33 @@ export default function Battle() {
     transition={{ duration: .5 }}
   >
     <div className="battleSection">
-      <h1>FIGHT!!!</h1>
-      <div>
-        <button
-          // battle(attacker, defender)
-          onClick={() => battle()}
-          >Start battle
-        </button>
+        <h1>FIGHT!!!</h1>
+        <div className="player">
+          <div className="playerShipPic">
+            <img src={playerShipPic}/>
+          </div>
+        </div>
+        <div className="enemy">
+          <div className="whaleBossPic">
+            <img src={whaleBossPic}/>
+          </div>
+        </div>
+          <div className="actionArea">
+            <button
+            // battle(attacker, defender)
+            onClick={() => battle()}
+            >Start battle
+          </button>
 
-        <button
-          >Next Round
-        </button>
+          <button
+            >Next Round
+          </button>
 
-        <button
-          >Retreat!
-        </button>
-      </div>
-      </div>
+          <button
+            >Retreat!
+          </button>
+        </div>
+    </div>
       </motion.div>
   );
 }
