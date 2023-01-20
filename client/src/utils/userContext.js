a6fe54fd9a052f4b8b270886665dc73fac6a61e2
Gordon-Magill <gordon.magill@gmail.com>
2023-01-19 18:56:38 -0800

    Changed initial state to be compatible with userReducer

--- client/src/utils/userContext.js
+++ client/src/utils/userContext.js
@@ -1,22 +1,15 @@
-import React, {useState, useContext} from 'react';
-import { useContext } from 'react';
+import React, {createContext, useContext} from 'react';
 
 // Defining a global state for user information
 // To be derived from Apollo login/addUser {token, **user**} responses
-const UserContext = React.createContext()
+export const UserContext = createContext()
 
-function useUserContext() {
+export function useUserContext() {
     useContext(UserContext)
 }
 
-function UserProvider(props) {
+export function UserProvider(props) {
     return (
-        <UserContext.Provider value={{loggedInUser: loggedInUser}} {...props} />
+        <UserContext.Provider value={null} {...props} />
     )
-}
-
-module.exports = {
-    UserContext,
-    useUserContext,
-    UserProvider
 }
