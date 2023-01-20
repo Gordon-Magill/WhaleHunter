fb00483f23888d8e4cc24cd63c79f2db8f2bd337
Gordon-Magill <gordon.magill@gmail.com>
2023-01-19 20:28:36 -0800

    Added login state conditional formatting to sidebar

--- client/src/pages/page-components/Sidebar.jsx
+++ client/src/pages/page-components/Sidebar.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { userReducer } from "../../utils/reducers";
+import { useUserContext } from "../../utils/userContext";
 
 import { Link } from "react-router-dom";
 
@@ -6,50 +8,72 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/01-logos/logo-gray.png";
 
 export default function Sidebar() {
+  const initialState = useUserContext();
+  const [userState, userDispatch] = useReducer(userReducer, initialState);
+
   return (
-    <aside
-    className="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-gray-700"
-  >
-    <div className="sidebar-header flex items-center justify-center py-4">
-      <div className="inline-flex">
-        <img src={Logo}/>
+    <aside className="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-gray-700">
+      <div className="sidebar-header flex items-center justify-center py-4">
+        <div className="inline-flex">
+          <img src={Logo} />
+        </div>
       </div>
-    </div>
-    <div className="sidebar px-4 py-6">
-      <ul className="flex flex-col w-full">
-        <li className="my-px">
-          <Link className="nav-link active" to="/dashboard">
-                  Dashboard
-          </Link>
-        </li>
-        <li className="my-px">
-          <Link className="nav-link active" to="/harbor">
-                  Harbor
-          </Link>
-        </li>
-        <li className="my-px">
-          <Link className="nav-link active" to="/gallery">
-                  Gallery
-          </Link>
-        </li>
-        <li className="my-px">
-          <Link className="nav-link active" to="/battle">
-                  Battle
-          </Link>
-        </li>
-        {/* Make this disappear upong login */}
-        <li className="my-px">
-          <Link className="nav-link active" to="/login">
+      <div className="sidebar px-4 py-6">
+        {userState?.username ? (
+          <>
+            <p>Hello {userState.username}!</p>
+          </>
+        ) : (
+          <>
+            <p>No user logged in</p>
+          </>
+        )}
+        <ul className="flex flex-col w-full">
+          <li className="my-px">
+            <Link className="nav-link active" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li className="my-px">
+            <Link className="nav-link active" to="/harbor">
+              Harbor
+            </Link>
+          </li>
+          <li className="my-px">
+            <Link className="nav-link active" to="/gallery">
+              Gallery
+            </Link>
+          </li>
+          <li className="my-px">
+            <Link className="nav-link active" to="/battle">
+              Battle
+            </Link>
+          </li>
+          {/* Make this disappear upong login */}
+          {userState?.username ? (
+            <>
+              <li className="my-px">
+                <Link className="nav-link active" to="/signup">
+                  Log out
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="my-px">
+                <Link className="nav-link active" to="/login">
                   Login
-          </Link>
-        </li>
-        <li className="my-px">
-          <Link className="nav-link active" to="/signup">
+                </Link>
+              </li>
+              <li className="my-px">
+                <Link className="nav-link active" to="/signup">
                   Sign up
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </aside>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </aside>
   );
 }
