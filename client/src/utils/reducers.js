997fb0c796d323575f25f3869ca29615a6a5a671
Gordon-Magill <gordon.magill@gmail.com>
2023-01-19 18:49:11 -0800

    Updated userReducer with actions

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,8 +1,20 @@
+import {LOGIN, LOGOUT} from './actions'
+
 export function userReducer(state, action) {
   switch (action.type) {
-    case 'LOGIN_USER': {
+    case LOGIN: {
         const userInfo = action.payload.user || null
+        console.log('userReducer LOGIN set user info as: ', userInfo)
 
+        return {
+            ...state,
+            userInfo
+        }
+    }
+    case LOGOUT: {
+        const userInfo = null
+        console.log('userReducer LOGOUT set user info as null: ',userInfo)
+        
         return {
             ...state,
             userInfo
