c67e756f61edef76adb1854c8126c8f46e18a39a
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-21 22:01:34 -0700

    Added query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,4 +14,18 @@ export const QUERY_ME = gql`
   }
 `;
 
-//todo: single monster/random single monster query 
+export const QUERY_SINGLE_ICON = gql`
+  query getSingleIcon($iconId: ID!) {
+    icon(iconId: $iconId) {
+      _id
+      objectType
+      img {
+        _id
+        data
+        contentType
+      }
+    }
+  }
+`;
+
+//todo: single monster/random single monster query
