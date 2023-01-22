02110c7198feab0f13ffbf9ba6ac9809d8578b0f
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-22 00:41:16 -0700

    Basic button function to pass selected moonster to gotoBattle function.

--- client/src/pages/page-components/MonsterList.jsx
+++ client/src/pages/page-components/MonsterList.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
+import goToBattle from '../../components/goToBattle'
+
 const MonsterList = ({ monsters, title }) => {
   if (!monsters.length) {
     return <h3>No Monsters Yet</h3>;
   }
 
   return (
-    <div>
+    <div className="monsters">
       <h3>{title}</h3>
       {monsters &&
         monsters.map((monster) => (
           <div key={monster._id} className="card mb-3">
             <h4 className="card-header bg-primary text-light p-2 m-0">
               {monster.name}
-            </h4>
+                </h4>
+                <button
+                    onClick={() => goToBattle(monster)}>Fight {monster.name}</button>
           </div>
         ))}
     </div>
