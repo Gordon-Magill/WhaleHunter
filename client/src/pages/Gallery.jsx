168739bfd80a1cead2d365ac631634d033d3603d
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-22 13:44:22 -0700

    TODOs for the gallery page

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -4,18 +4,22 @@ import { motion } from "framer-motion";
 import {TRANSITION_SPEED} from '../utils/transitionSpeed'
 
 
+
+// TODO: Query the user's ship
+// TODO: Query all of user's successfull conquests (monsters)
+
 export default function Gallery() {
   return (
     <motion.div
-    className="container text-center"
+    classNameName="container text-center"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: TRANSITION_SPEED }}
   >
-    <div className="gallery">
-          <h1>The Gallery</h1>
-          <p>Here's your stuff</p>
+    <div classNameName="gallery">
+        <h1>The Gallery</h1>
+
       </div>
       </motion.div>
   );
