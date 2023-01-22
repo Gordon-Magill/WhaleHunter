187fd12f58b746f05b93d2aeeba288d377a102f2
Gordon-Magill <gordon.magill@gmail.com>
2023-01-21 16:06:16 -0800

    Fixed model imports to model index, implemented basic item seeding

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const User = require("./User");
-const Item = require("./Item");
-const Ship = require("./Ship");
-const Monster = require("./Monster");
+const {Item} = require("./Item");
+const {Ship} = require("./Ship");
+const {Monster} = require("./Monster");
 
 
 module.exports = { User, Ship, Item, Monster};
