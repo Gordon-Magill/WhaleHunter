fe93499af7f7b5bdfd2d1d1b6fdf2c06eb428d8e
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-01-22 20:15:39 -0700

    Establishing non-error environment

--- client/src/pages/page-components/MonsterList.jsx
+++ client/src/pages/page-components/MonsterList.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
-import Monster from '../../components/Monster';
 // Importing content for userReducer
 import { useUserStateContext, useUserDispatchContext} from "../../utils/userContext";
 import { LOAD_MONSTER } from '../../utils/actions';
 import { UNLOAD_MONSTER } from '../../utils/actions';
 import { QUERY_MONSTERS } from '../../utils/queries';
 
-function MonsterList() {
+const MonsterList = ({ monsters, title }) => {
   const state = useUserStateContext();
   const dispatch = useUserDispatchContext();
 
@@ -16,31 +15,26 @@ function MonsterList() {
 
   const { loading, data } = useQuery(QUERY_MONSTERS);
 
-  useEffect(() => {
-    if (data) {
-      dispatch({
-        type: LOAD_MONSTER,
-        payload: data.monsterInfo,
-      });
-    }
-  }, [data, dispatch]);
+  if (!monsters.length) {
+    return <h3>No Monsters Yet</h3>;
+  }
+
 
   return (
-    <div className="my-2">
-      <h2>The Monsters</h2>
-      {state.monsterInfo.length ? (
-        <div className="flex-row">
-            <Monster
-              key={monsterInfo._id}
-              _id={monsterInfo._id}
-              name={monsterInfo.name}
-            />
-        </div>
-      ) : (
-        <h3>No monsters to fight!</h3>
-      )}
+    <div className="monsters">
+      <h3>{title}</h3>
+      {monsters &&
+        monsters.map((monster) => (
+          <div key={monster._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {monster.name}
+                </h4>
+                <button
+                    onClick={() => goToBattle(monster)}>Fight {monster.name}</button>
+          </div>
+        ))}
     </div>
   );
-}
+};
 
 export default MonsterList;
