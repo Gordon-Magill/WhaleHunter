4db1c38a60c9e9c612793a9d4a58dd91ec14476d
Gordon-Magill <gordon.magill@gmail.com>
2023-01-22 22:15:34 -0800

    Removed traces of Icon model

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const User = require("./User");
 const {Ship} = require("./Ship");
 const {Monster} = require("./Monster");
-const {Icon} = require("./Icon")
 
 
-module.exports = { User, Ship, Monster, Icon};
+module.exports = { User, Ship, Monster};
