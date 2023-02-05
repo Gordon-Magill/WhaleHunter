1e72ae38f91dc2e50cfc42aa3985accb2e96ba93
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2023-02-04 21:54:24 -0700

    Cleaning up unused code

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -15,249 +15,251 @@ import { TRANSITION_SPEED } from "../utils/transitionSpeed";
 // Test player picture
 // import playerShipPic from "../assets/future_ships/future_1.png";
 // Test HP Value for PLayer
-const playerHP = atkCurrentHp;
-const bossHP = defCurrentHp;
 
 // A generic class that can be a player or a monster
 
 // // Here begins the battle script
 
-var battleState = 0;
-var atkCurrentHp;
-var atkCurrentArmor;
-var atkCurrentShield;
-var defCurrentHp;
-var defCurrentArmor;
-var defCurrentShield;
-var roundCounter = 0;
-var battleMsgOne;
-var battleMsgTwo;
+// var battleState = 0;
+// var atkCurrentHp;
+// var atkCurrentArmor;
+// var atkCurrentShield;
+// var defCurrentHp;
+// var defCurrentArmor;
+// var defCurrentShield;
+// var roundCounter = 0;
+// var battleMsgOne;
+// var battleMsgTwo;
 
-function getRandomInt(num) {
-  return Math.floor(Math.random() * num);
-}
+// var playerHP = 100;
+// var monsterHP = 100;
 
-function diceRoll(modifier) {
-  // 3d6 roll. modifier argument is added or subtracted from roll to decrease or increase difficulty. higher roll = better
-  // todo: implement critical failure and success (match 3 and 18)
-  let roll = getRandomInt(6) + getRandomInt(6) + getRandomInt(6) + modifier;
-  return roll;
-}
 
-const updateMsg = (int) => {
-  if (int === 1) {
-    document.getElementById("msgOne").innerHTML = battleMsgOne;
-  } else if (int === 2) {
-    document.getElementById("msgTwo").innerHTML = battleMsgTwo;
-  }
-};
+// function getRandomInt(num) {
+//   return Math.floor(Math.random() * num);
+// }
 
-// this function manages a half round of battle where damage is dealt
-function round([atkPower, defHp, defArmor, defShield]) {
-  console.log("Round function called");
-  console.log(
-    `Attack power ${atkPower} hitting ${defHp} hp, ${defArmor} armor, ${defShield} shield`
-  );
-  let remainingPower = atkPower;
+// function diceRoll(modifier) {
+//   // 3d6 roll. modifier argument is added or subtracted from roll to decrease or increase difficulty. higher roll = better
+//   // todo: implement critical failure and success (match 3 and 18)
+//   let roll = getRandomInt(6) + getRandomInt(6) + getRandomInt(6) + modifier;
+//   return roll;
+// }
 
-  // hit shield first
-  if (defShield > 0) {
-    // this will be negative if defShield > atkPower, meaning no damage will get through the shield this turn
-    remainingPower = atkPower - defShield;
+// const updateMsg = (int) => {
+//   if (int === 1) {
+//     document.getElementById("msgOne").innerHTML = battleMsgOne;
+//   } else if (int === 2) {
+//     document.getElementById("msgTwo").innerHTML = battleMsgTwo;
+//   }
+// };
 
-    defShield = defShield - atkPower;
+// // this function manages a half round of battle where damage is dealt
+// function round([atkPower, defHp, defArmor, defShield]) {
+//   console.log("Round function called");
+//   console.log(
+//     `Attack power ${atkPower} hitting ${defHp} hp, ${defArmor} armor, ${defShield} shield`
+//   );
+//   let remainingPower = atkPower;
 
-    // if decreased below 0, set to 0
-    if (defShield < 0) {
-      defShield = 0;
-    }
-  }
+//   // hit shield first
+//   if (defShield > 0) {
+//     // this will be negative if defShield > atkPower, meaning no damage will get through the shield this turn
+//     remainingPower = atkPower - defShield;
 
-  // hit armor+hp with damage mitigation if armor > 0
-  if (remainingPower > 0) {
-    if (defArmor > 0) {
-      let armor = defArmor;
-      defArmor = defArmor - Math.floor(remainingPower / 2);
-      defHp = defHp - Math.floor(remainingPower / 4);
-      remainingPower = remainingPower - armor;
-    }
-  }
+//     defShield = defShield - atkPower;
 
-  if (remainingPower > 0) {
-    if (defHp > 0) {
-      defHp = defHp - remainingPower;
-    }
-  }
-  console.log(`${defHp} hp, ${defArmor} armor, ${defShield} shield`);
-  return [defHp, defArmor, defShield];
-}
+//     // if decreased below 0, set to 0
+//     if (defShield < 0) {
+//       defShield = 0;
+//     }
+//   }
 
-// initialize the battle state, get values for attacker and defender, wrap other functions in this function
-function startBattle(attacker, defender) {
-  if (battleState === 0) {
-    battleMsgOne = "";
-    battleMsgTwo = "";
+//   // hit armor+hp with damage mitigation if armor > 0
+//   if (remainingPower > 0) {
+//     if (defArmor > 0) {
+//       let armor = defArmor;
+//       defArmor = defArmor - Math.floor(remainingPower / 2);
+//       defHp = defHp - Math.floor(remainingPower / 4);
+//       remainingPower = remainingPower - armor;
+//     }
+//   }
 
-    console.log(`Battle state initiated! ${attacker.name} vs ${defender.name}`);
-    battleState = 1;
+//   if (remainingPower > 0) {
+//     if (defHp > 0) {
+//       defHp = defHp - remainingPower;
+//     }
+//   }
+//   console.log(`${defHp} hp, ${defArmor} armor, ${defShield} shield`);
+//   return [defHp, defArmor, defShield];
+// }
 
-    atkCurrentHp = attacker.healthCurrent;
-    atkCurrentArmor = attacker.armorCurrent;
-    atkCurrentShield = attacker.shieldCurrent;
+// // initialize the battle state, get values for attacker and defender, wrap other functions in this function
+// function startBattle(attacker, defender) {
+//   if (battleState === 0) {
+//     battleMsgOne = "";
+//     battleMsgTwo = "";
 
-    defCurrentHp = defender.health;
-    defCurrentArmor = defender.armor;
-    defCurrentShield = defender.shield;
+//     console.log(`Battle state initiated! ${attacker.name} vs ${defender.name}`);
+//     battleState = 1;
 
-    roundCounter = 1;
-    battleMsgOne = "Battle commenced! Press 'Next Round' to continue.";
-    updateMsg(1);
-    // nextRound(attacker, defender);
-  } else {
-    console.log("Battle already initiated!");
-  }
-}
+//     atkCurrentHp = attacker.healthCurrent;
+//     atkCurrentArmor = attacker.armorCurrent;
+//     atkCurrentShield = attacker.shieldCurrent;
 
-// if nextRound button is pressed call this function
-function nextRound(attacker, defender) {
-  // reset battle messages
-  battleMsgOne = "";
-  battleMsgTwo = "";
-  updateMsg(1);
-  updateMsg(2);
-  // Render Round number to page
-  let currentRound = roundCounter;
-  let roundEle = document.getElementById("currentRound");
-  roundEle.innerHTML = "Round: " + currentRound;
+//     defCurrentHp = defender.health;
+//     defCurrentArmor = defender.armor;
+//     defCurrentShield = defender.shield;
 
-  if (atkCurrentHp >= 1 && defCurrentHp >= 1) {
-    // roll accuracy vs evasion to determine hit or miss
-    if (diceRoll(attacker.accuracy) > diceRoll(defender.evasion)) {
-      let roundResult = round([
-        attacker.attackPower,
-        defCurrentHp,
-        defCurrentArmor,
-        defCurrentShield,
-      ]);
+//     roundCounter = 1;
+//     battleMsgOne = "Battle commenced! Press 'Next Round' to continue.";
+//     updateMsg(1);
+//     // nextRound(attacker, defender);
+//   } else {
+//     console.log("Battle already initiated!");
+//   }
+// }
 
-      // update values
-      defCurrentHp = roundResult[0];
-      defCurrentArmor = roundResult[1];
-      defCurrentShield = roundResult[2];
+// // if nextRound button is pressed call this function
+// function nextRound(attacker, defender) {
+//   // reset battle messages
+//   battleMsgOne = "";
+//   battleMsgTwo = "";
+//   updateMsg(1);
+//   updateMsg(2);
+//   // Render Round number to page
+//   let currentRound = roundCounter;
+//   let roundEle = document.getElementById("currentRound");
+//   roundEle.innerHTML = "Round: " + currentRound;
 
-      // check defender hp
-      if (defCurrentHp < 1) {
-        // defender has been defeated
-        battleMsgOne = `You defeated ${defender.name}!`;
-        updateMsg(1);
-        console.log(battleMsgOne);
-        // end battle state
+//   if (atkCurrentHp >= 1 && defCurrentHp >= 1) {
+//     // roll accuracy vs evasion to determine hit or miss
+//     if (diceRoll(attacker.accuracy) > diceRoll(defender.evasion)) {
+//       let roundResult = round([
+//         attacker.attackPower,
+//         defCurrentHp,
+//         defCurrentArmor,
+//         defCurrentShield,
+//       ]);
 
-        endBattle("win");
-        return;
-      }
+//       // update values
+//       defCurrentHp = roundResult[0];
+//       defCurrentArmor = roundResult[1];
+//       defCurrentShield = roundResult[2];
 
-      battleMsgOne = `${attacker.name} damaged ${defender.name}!`;
-      updateMsg(1);
-      console.log(battleMsgOne);
-    } else {
-      // it's a miss!
-      battleMsgOne = `${attacker.name} missed ${defender.name}...`;
-      updateMsg(1);
-      console.log(battleMsgOne);
-    }
+//       // check defender hp
+//       if (defCurrentHp < 1) {
+//         // defender has been defeated
+//         battleMsgOne = `You defeated ${defender.name}!`;
+//         updateMsg(1);
+//         console.log(battleMsgOne);
+//         // end battle state
 
-    if (diceRoll(defender.accuracy) > diceRoll(attacker.evasion)) {
-      let roundResult = round([
-        defender.attackPower,
-        atkCurrentHp,
-        atkCurrentArmor,
-        atkCurrentShield,
-      ]);
+//         endBattle("win");
+//         return;
+//       }
 
-      // update values
-      atkCurrentHp = roundResult[0];
-      atkCurrentArmor = roundResult[1];
-      atkCurrentShield = roundResult[2];
+//       battleMsgOne = `${attacker.name} damaged ${defender.name}!`;
+//       updateMsg(1);
+//       console.log(battleMsgOne);
+//     } else {
+//       // it's a miss!
+//       battleMsgOne = `${attacker.name} missed ${defender.name}...`;
+//       updateMsg(1);
+//       console.log(battleMsgOne);
+//     }
 
-      // check attacker hp
-      if (atkCurrentHp < 1) {
-        // attacker has been defeated
-        battleMsgTwo = `You were defeated...`;
-        console.log(battleMsgTwo);
-        // end battle state
-        endBattle("lose");
-        return;
-      }
+//     if (diceRoll(defender.accuracy) > diceRoll(attacker.evasion)) {
+//       let roundResult = round([
+//         defender.attackPower,
+//         atkCurrentHp,
+//         atkCurrentArmor,
+//         atkCurrentShield,
+//       ]);
 
-      battleMsgTwo = `${defender.name} damaged ${attacker.name}!`;
-      updateMsg(2);
-      console.log(battleMsgTwo);
-    } else {
-      // it's a miss!
-      battleMsgTwo = `${defender.name} missed ${attacker.name}...`;
-      updateMsg(2);
-      console.log(battleMsgTwo);
-    }
+//       // update values
+//       atkCurrentHp = roundResult[0];
+//       atkCurrentArmor = roundResult[1];
+//       atkCurrentShield = roundResult[2];
 
-    // return some data to the page
+//       // check attacker hp
+//       if (atkCurrentHp < 1) {
+//         // attacker has been defeated
+//         battleMsgTwo = `You were defeated...`;
+//         console.log(battleMsgTwo);
+//         // end battle state
+//         endBattle("lose");
+//         return;
+//       }
 
-    roundCounter++;
-    console.log("Round: " + roundCounter);
+//       battleMsgTwo = `${defender.name} damaged ${attacker.name}!`;
+//       updateMsg(2);
+//       console.log(battleMsgTwo);
+//     } else {
+//       // it's a miss!
+//       battleMsgTwo = `${defender.name} missed ${attacker.name}...`;
+//       updateMsg(2);
+//       console.log(battleMsgTwo);
+//     }
 
-    console.log(
-      `Round results: attacker has ${atkCurrentHp} hp, defender has ${defCurrentHp} hp`
-    );
-  }
-  // Append final text to the page element
-}
+//     // return some data to the page
+
+//     roundCounter++;
+//     console.log("Round: " + roundCounter);
+
+//     console.log(
+//       `Round results: attacker has ${atkCurrentHp} hp, defender has ${defCurrentHp} hp`
+//     );
+//   }
+//   // Append final text to the page element
+// }
 
 // if retreat button is pressed call this function
-function retreat() {
-  // end battle state
-  if (battleState === 1) endBattle("lose");
-}
+// function retreat() {
+//   // end battle state
+//   if (battleState === 1) endBattle("lose");
+// }
 
-function endBattle(outcome) {
-  console.log("Battle state ended!");
-  battleState = 0;
-  if (outcome === "win") {
-  } else if (outcome === "lose") {
-  } else {
-    console.log("Error in battle outcome");
-  }
-}
+// function endBattle(outcome) {
+//   console.log("Battle state ended!");
+//   battleState = 0;
+//   if (outcome === "win") {
+//   } else if (outcome === "lose") {
+//   } else {
+//     console.log("Error in battle outcome");
+//   }
+// }
 
 // // Here ends the battle script
 
 //debug attacker and defender
 
-var attacker = {
-  name: "Debug Ship",
-  imageID: null,
-  attackPower: 4,
-  health: 14,
-  healthCurrent: 14,
-  armor: 5,
-  armorCurrent: 5,
-  shield: 5,
-  shieldCurrent: 5,
-  accuracy: 29,
-  initiative: 3,
-  evasion: 21,
-};
+// var attacker = {
+//   name: "Debug Ship",
+//   imageID: null,
+//   attackPower: 4,
+//   health: 14,
+//   healthCurrent: 14,
+//   armor: 5,
+//   armorCurrent: 5,
+//   shield: 5,
+//   shieldCurrent: 5,
+//   accuracy: 29,
+//   initiative: 3,
+//   evasion: 21,
+// };
 
-const defender = {
-  name: "Debug Monster",
-  imageID: null,
-  attackPower: 4,
-  health: 19,
-  armor: 3,
-  shield: 0,
-  accuracy: 21,
-  initiative: 15,
-  evasion: 5,
-};
+// const defender = {
+//   name: "Debug Monster",
+//   imageID: null,
+//   attackPower: 4,
+//   health: 19,
+//   armor: 3,
+//   shield: 0,
+//   accuracy: 21,
+//   initiative: 15,
+//   evasion: 5,
+// };
 
 class Combatant {
   constructor(obj) {
@@ -309,6 +311,9 @@ class BattleClass {
     console.log("Initiating a round of combat!");
     console.log("Player HP: ", this.player.health);
     console.log("Monster HP: ", this.monster.health);
+    playerHP = this.player.health;
+    monsterHP = this.monster.health;
+
     // If the player is alive, attack
     if (this.player.isAlive()) {
       this.player.attack(this.monster);
@@ -376,10 +381,7 @@ export default function Battle() {
     victoryStateSetter
   );
 
-  // TODO: Dynamically obtain starting health for both
-  let playerHealth = (playerState.health / playerShipInfo.health) * 100;
-  // let playerHealth = (playerState.health / 10) * 100;
-  let monsterHealth = (monsterState.health / monsterObj.health) * 100;
+
 
   return (
     <motion.div
@@ -406,20 +408,22 @@ export default function Battle() {
           Battle is over! Collect your prize!
         </button>
       )}
+
+
+
       <div className="flex flex-row justify-around items-center battleSection">
         <div className="player">
           <img src={playerState.imagePath} />
           <div className="relative">
             <motion.div
               initial={{ scaleX: "0%" }}
-              animate={{ scaleX: `${playerHealth}%` }}
+              animate={{ scaleX: `${playerState.health}%` }}
               className=" my-3 rounded-lg text-white text-2xl bg-teal-600"
             >
               {playerState.health}HP
             </motion.div>
           </div>
         </div>
-
         <div className="actionArea">
           <div className="actionText">
             <p id="currentRound"></p>
@@ -451,7 +455,7 @@ export default function Battle() {
           <img src={monsterState.imagePath} />
           <motion.div
             initial={{ scaleX: "0%" }}
-            animate={{ scaleX: `${monsterHealth}%` }}
+            animate={{ scaleX: `${monsterState.health}%` }}
             className=" my-3 rounded-lg text-white text-2xl bg-teal-900"
           >
             {monsterState.health}HP
